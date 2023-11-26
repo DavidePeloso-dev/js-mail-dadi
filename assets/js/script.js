84c187085fadcdab92e618f8a440ecc78a77ee9f
DavidePeloso-dev <davide.peloso.e@gmail.com>
2023-11-26 11:51:18 +0100

    complete log in page (Add validation)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,53 @@
-const userMail= ['davide@gmail.com', 'fabio@gmail.com', 'luca@gmail.com'];
-const userPassword = ['mangia', 'prega', 'ama'];
+const usersMail= ['davide@gmail.com', 'fabio@gmail.com', 'luca@gmail.com'];
+const usersPassword = ['mangia', 'prega', 'ama'];
+
+const inputMail = document.querySelector('#email')
+const inputPassword = document.querySelector('#password');
+
 
 const errorModal = document.querySelector('.bg-modal');
 
-const Access = document.querySelector('#access_form');
+const accessForm = document.querySelector('#access_form');
 
-Access.addEventListener('submit', function(sub){
+accessForm.addEventListener('submit', function(sub){
     sub.preventDefault();
-    const userMailAccess = document.querySelector('#email').value;
-    const userPasswordAccess = document.querySelector('#password').value;
+    sub.stopPropagation();
+    const userMailAccess = inputMail.value;
+    const userPasswordAccess = inputPassword.value;
 
-    if(userMail.includes(userMailAccess)){
-        for (let i = 0; i < userMail.length; i++) {
-            const mail = userMail[i];
+    if(usersMail.includes(userMailAccess)){
+        for (let i = 0; i < usersMail.length; i++) {
+            const mail = usersMail[i];
             if ((userMailAccess === mail)) {
-                if(userPasswordAccess === userPassword[i]){
+                if(userPasswordAccess === usersPassword[i]){
                     console.log('access');
+                    inputMail.classList.remove('error', 'is-invalid')
+                    inputPassword.classList.remove('error', 'is-invalid')
                 } else {
                     console.log('something wrong');
                     errorModal.classList.remove('d-none');
-                    document.querySelector('.card-text').innerHTML = 'Try to check your credentials';
+                    document.querySelector('.card-title').innerHTML = 'Try to check your credentials';
+                    accessForm.classList.add('was-validated');
+                    inputMail.classList.add('error', 'is-invalid')
+                    inputPassword.classList.add('error', 'is-invalid')
                 }
             } 
         }
     } else {
         errorModal.classList.remove('d-none');
-        document.querySelector('.card-text').innerHTML = 'You are NOT registered';
+        document.querySelector('.card-title').innerHTML = 'You are NOT registered';
+        accessForm.classList.add('was-validated');
+        inputMail.classList.add('error', 'is-invalid')
+        inputPassword.classList.add('error', 'is-invalid')
     }
+})
+
+inputMail.addEventListener('click', function(){
+    inputMail.classList.remove('error', 'is-invalid')
+})
+
+inputPassword.addEventListener('click', function(){
+    inputPassword.classList.remove('error', 'is-invalid')
 })
 
 const closeModal = document.querySelector('#close_modal');
