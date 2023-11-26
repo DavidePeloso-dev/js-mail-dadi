2785ce31047dacca31df0cab91c7be0a3ed06e79
DavidePeloso-dev <davide.peloso.e@gmail.com>
2023-11-26 12:02:58 +0100

    Add comments in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,40 @@
+// define the mail and password in the sistem
 const usersMail= ['davide@gmail.com', 'fabio@gmail.com', 'luca@gmail.com'];
 const usersPassword = ['mangia', 'prega', 'ama'];
 
+// declare the inputs in the form
 const inputMail = document.querySelector('#email')
 const inputPassword = document.querySelector('#password');
 
-
+// declare the error modal
 const errorModal = document.querySelector('.bg-modal');
 
+// declare the all form
 const accessForm = document.querySelector('#access_form');
 
+// validating the submit
 accessForm.addEventListener('submit', function(sub){
+    // prevent defoult function
     sub.preventDefault();
     sub.stopPropagation();
+    // declare the value in the imputs
     const userMailAccess = inputMail.value;
     const userPasswordAccess = inputPassword.value;
 
+    // is the mail in the sistem?
     if(usersMail.includes(userMailAccess)){
+        // Yes
         for (let i = 0; i < usersMail.length; i++) {
             const mail = usersMail[i];
+            // if so, is the password the correct one?
             if ((userMailAccess === mail)) {
                 if(userPasswordAccess === usersPassword[i]){
+                    // Yes
                     console.log('access');
                     inputMail.classList.remove('error', 'is-invalid')
                     inputPassword.classList.remove('error', 'is-invalid')
                 } else {
+                    // No
                     console.log('something wrong');
                     errorModal.classList.remove('d-none');
                     document.querySelector('.card-title').innerHTML = 'Try to check your credentials';
@@ -34,6 +45,7 @@ accessForm.addEventListener('submit', function(sub){
             } 
         }
     } else {
+        // No
         errorModal.classList.remove('d-none');
         document.querySelector('.card-title').innerHTML = 'You are NOT registered';
         accessForm.classList.add('was-validated');
@@ -42,6 +54,7 @@ accessForm.addEventListener('submit', function(sub){
     }
 })
 
+//remove the error validatin
 inputMail.addEventListener('click', function(){
     inputMail.classList.remove('error', 'is-invalid')
 })
@@ -50,6 +63,8 @@ inputPassword.addEventListener('click', function(){
     inputPassword.classList.remove('error', 'is-invalid')
 })
 
+
+//close the error modal
 const closeModal = document.querySelector('#close_modal');
 
 closeModal.addEventListener('click', function(){
